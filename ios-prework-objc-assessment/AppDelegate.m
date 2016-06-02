5f218276227e75551e31dcfab2068d66b2ec4feb
SusanZheng15 <zheng.susan12@gmail.com>
2016-06-02 09:14:45 -0400

    Done

--- ios-prework-objc-assessment/AppDelegate.m
+++ ios-prework-objc-assessment/AppDelegate.m
@@ -14,9 +14,57 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    NSArray *numbersArray = @[];
+    NSArray *lettersArray = @[];
+    NSDictionary *alphabetDictionary = @{};
+        
+    numbersArray = [self generateArrayOfNumbers];
+    lettersArray = [self generateArrayOfletters];
+    alphabetDictionary = [self generateNumbersAndLettersDictionaryWithNumbers:numbersArray letters:lettersArray];
+        
+    NSLog(@"%@", alphabetDictionary);
+        
+    NSMutableArray *mutableLetterArray = [lettersArray mutableCopy];
+    [mutableLetterArray removeAllObjects];
     
+    for (NSNumber *key in alphabetDictionary)
+    {
+        [mutableLetterArray addObject:alphabetDictionary[key]];
+    }
+        
+    NSLog(@"%@\n%@", numbersArray, mutableLetterArray);
     return YES;
 }
+-(NSArray *)generateArrayOfNumbers
+{
+    NSMutableArray *mutNumArray = [[NSMutableArray alloc]init];
+    for (NSInteger i = 1; i <= 26; i++)
+    {
+        NSNumber *num = @(i);
+        [mutNumArray addObject: num];
+    }
+    NSLog(@"%@", mutNumArray);
+    return mutNumArray;
+}
+-(NSArray *)generateArrayOfletters
+{
+    NSMutableArray *mutLetArray = [[NSMutableArray alloc] init];
 
+    for (char az = 'a'; az <= 'z'; az++)
+    {
+        NSString *letter = [NSString stringWithFormat:@"%c", az];
+        [mutLetArray addObject:letter];
+    }
+        NSLog(@"%@", mutLetArray);
+        return mutLetArray;
+}
+- (NSDictionary *) generateNumbersAndLettersDictionaryWithNumbers: (NSArray*)numbersArray letters: (NSArray *)lettersArray
+{
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc]init];
+        
+    [dict setObject:numbersArray forKey: lettersArray];
+    return dict;
+}
 @end
